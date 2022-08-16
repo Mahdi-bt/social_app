eaf6a7ad702f0b5c9f6b68c7bd6d3742e5b7c781
Mahdi bt <bentamansourtmahdi@gmail.com>
2022-08-16 15:52:25 +0100

    setup add new post feature

--- lib/layout/cubit/cubit.dart
+++ lib/layout/cubit/cubit.dart
@@ -1,8 +1,16 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:intl/intl.dart';
 import 'package:social_app/layout/cubit/states.dart';
+import 'package:social_app/models/postModel.dart';
+import 'package:social_app/models/userModel.dart';
 import 'package:social_app/modules/chat/contact_screen.dart';
 import 'package:social_app/modules/feed/feed_screen.dart';
 import 'package:social_app/modules/notifications/notification.dart';
@@ -13,6 +21,7 @@ import 'package:social_app/shared/local/CacheHelper.dart';
 class HomeCubit extends Cubit<HomeLayoutStates> {
   HomeCubit() : super(HomeInitialState());
   static HomeCubit get(context) => BlocProvider.of(context);
+
   int currentIndex = 0;
   List<IconData> listOfIcons = [
     Icons.home_rounded,
@@ -32,23 +41,107 @@ class HomeCubit extends Cubit<HomeLayoutStates> {
   }
 
   logOut(context) async {
-    CacheHelper.sharedPreferences.remove('uid').then((value) {
-      emit(HomeLogOutSuccesStates());
-      Navigator.of(context).pushReplacementNamed('/login');
-    }).catchError((onError) {
-      emit(HomeLogOutFailedSates());
-    });
-  }
-
-  logOutFromGoogle(context) async {
-    await googleSignIn.disconnect();
-    FirebaseAuth.instance.signOut().then((value) {
+    await FirebaseAuth.instance.signOut().then((value) {
       CacheHelper.sharedPreferences.remove('uid').then((value) {
         emit(HomeLogOutSuccesStates());
         Navigator.of(context).pushReplacementNamed('/login');
       }).catchError((onError) {
         emit(HomeLogOutFailedSates());
       });
+    });
+  }
+
+  logOutFromGoogle(context) async {
+    await googleSignIn.disconnect();
+    await FirebaseAuth.instance.signOut().then((value) {
+      CacheHelper.sharedPreferences.remove('uid').then((value) {
+        emit(HomeLogOutSuccesStates());
+        Navigator.of(context).pushReplacementNamed('/login');
+      }).catchError((onError) {
+        emit(HomeLogOutFailedSates());
+      });
+    });
+  }
+
+  UserModel? currentUser;
+  getCurrentUser({required String uid}) async {
+    emit(HomeGetUserDataLoadingState());
+    await FirebaseFirestore.instance
+        .collection("users")
+        .doc(uid)
+        .get()
+        .then((value) {
+      currentUser = UserModel.fromJson(value.data()!);
+      emit(HomeGetUserDataSuccesState());
+      print(value.data());
+    }).catchError((onError) {
+      emit(HomeGetUserDataFailedState());
+    });
+  }
+
+  File? postImage;
+
+  var picker = ImagePicker();
+
+  pickPostImage() async {
+    var pickedFile = await ImagePicker().pickImage(
+      source: ImageSource.gallery,
+    );
+
+    if (pickedFile != null) {
+      postImage = File(pickedFile.path);
+      debugPrint(pickedFile.path);
+      emit(HomePickImageSuccesState());
+    } else {
+      debugPrint('No image selected.');
+      emit(HomePickImageFailedState());
+    }
+  }
+
+  deletePostImage() async {
+    postImage = null;
+    emit(HomePostImageDeleteSuccfuly());
+  }
+
+  createPostWithPic({required String text}) async {
+    emit(HomeUploadPostLoadingStates());
+    FirebaseStorage.instance
+        .ref("posts_images/${postImage!.path.split('/').last}")
+        .putFile(postImage!)
+        .then((p0) {
+      p0.ref.getDownloadURL().then((value) {
+        createPost(text: text, imageUrl: value);
+      });
+    }).catchError((onError) {});
+  }
+
+  createPost({required String text, String imageUrl = ""}) async {
+    DateTime now = DateTime.now();
+    String formattedDate = DateFormat('kk:mm EEE d MMM').format(now);
+    PostModel postModel = PostModel(
+        mediaUrl: imageUrl,
+        postTime: formattedDate,
+        postTitle: text,
+        posterName: currentUser!.userName,
+        posterUid: currentUser!.uid,
+        posterPhotoUrl: currentUser!.profilePic);
+    FirebaseFirestore.instance
+        .collection("posts")
+        .add(postModel.toMap())
+        .then((value) {
+      deletePostImage();
+      emit(HomeUploadPostSuccesStates());
+    }).catchError((onError) {
+      emit(HomeUploadPostFailedStates());
+    });
+  }
+
+  List<PostModel> posts = [];
+  getPosts() {
+    FirebaseFirestore.instance.collection("posts").get().then((value) {
+      for (var element in value.docs) {
+        posts.add(PostModel.fromJson(element.data()));
+      }
     });
   }
 }
